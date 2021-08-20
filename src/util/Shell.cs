d4f62a639464990b95743067419ea17f32c44720
Christopher Granade <cgranade@cgranade.com>
2021-08-20 09:21:29 -0700

    World's worst which.

--- src/util/Shell.cs
+++ src/util/Shell.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 static class Shell
 {
+
     private static string RunRaw(string cmd, string[] args, bool capture)
     {
         var startInfo = new System.Diagnostics.ProcessStartInfo
@@ -57,7 +58,7 @@ static class Shell
 
     [SupportedOSPlatform("linux")]
     [SupportedOSPlatform("osx")]
-    static Process? Bash(string[] cmd, bool capture = false)
+    public static Process? Bash(string[] cmd, bool capture = false)
     {
         var startInfo = new System.Diagnostics.ProcessStartInfo
         {
@@ -70,6 +71,35 @@ static class Shell
             startInfo.ArgumentList.Add(arg);
         }
         return Process.Start(startInfo);
+    }
+
+    [SupportedOSPlatform("linux")]
+    [SupportedOSPlatform("osx")]
+    public static Process? InlineBashScript(string source, bool capture = false)
+    {
+        // Write the source to a temporary file.
+        var baseName = Path.ChangeExtension(Path.GetRandomFileName(), "sh");
+        var target = Path.Join(Path.GetTempPath(), baseName);
+        File.WriteAllText(target, source.ReplaceLineEndings("\n"));
+        var process = Process.Start(new ProcessStartInfo
+        {
+            FileName = "bash",
+            Arguments = target,
+            RedirectStandardError = capture,
+            RedirectStandardOutput = capture
+        });
+        if (process != null)
+        {
+            process.Exited += (sender, args) =>
+            {
+                try
+                {
+                    File.Delete(target);
+                }
+                catch { }
+            };
+        }
+        return process;
     }
 
     public static bool Prompt(string question, string? detail = null, bool defaultResponse = true)
@@ -112,18 +142,48 @@ static class Shell
         if (OperatingSystem.IsWindows())
         {
             process = Batch(new[] { "where", cmd, "2>", "nul" }, capture: true);
+            process?.WaitForExit();
+            path = process?.StandardOutput?.ReadToEnd()?.Trim()?.SplitLines()?[0];
         }
         else if (OperatingSystem.IsLinux() || OperatingSystem.IsMacOS())
         {
-            process = Bash(new[] { $"which {cmd}" }, capture: true);
+            // What we have to do to make sure we can call which in a way
+            // that reflects updates to ~/.bashrc is a bit convoluted.
+            // If we just call bash -c which, we'll only get definitions that
+            // were there when the whole installer started.
+            //
+            // On the other hand, if we call bash -l -c which, then we'll get
+            // all the MOTD printouts in our stdout capture along with the
+            // path we actually want.
+            //
+            // Thus, what we do to get bash to tell us where it looks for
+            // something is to write to a file and then read that file back
+            // out. The most reliable way we've found to do so is to write
+            // a new script that does this for us, then to call that script.
+            //
+            // It's ludicrously indirect, but it seems to do pretty well at
+            // detecting what ★will★ be on PATH after the next bash session
+            // starts, rather than just what is currently on path.
+            var tempFile = Path.GetTempFileName();
+            process = Shell.InlineBashScript(
+                @$"
+                bash -i -c 'which {cmd} > {tempFile}'
+                ",
+                capture: true
+            );
+            process?.WaitForExit();
+            path = File.ReadAllText(tempFile).Trim();
+            try
+            {
+                File.Delete(tempFile);
+            }
+            catch { }
         }
         else
         {
             throw new Exception("Unsupported on current platform.");
         }
 
-        process?.WaitForExit();
-        path = process?.StandardOutput?.ReadToEnd()?.Trim()?.SplitLines()?[0];
         return process?.ExitCode == 0;
     }
 
@@ -166,6 +226,9 @@ static class Shell
 
     public static void WriteError(string text) =>
         WriteLineInColor(text, ConsoleColor.Red, error: true);
+    
+    public static void WriteWarning(string text) =>
+        WriteLineInColor(text, ConsoleColor.Yellow, error: true);
 
     public static void WriteInfo(string text) =>
         WriteLineInColor(text, ConsoleColor.Blue);
